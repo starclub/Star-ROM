be53328a5a6d5e12be58f3577dd642d48d0f28a8
starclub <frankfan120@gmail.com>
2016-04-08 10:11:16 -0400

    Remade boot.bat

--- boot.bat
+++ boot.bat
@@ -1,3 +1,25 @@
+@echo off
+color 0a
+set c=0
+:a
+if %c%==3 goto startE
+echo %c%
 echo The following project is powered by Star-ROM (C) 2016 Star Club
-pause
+echo Loading.
+echo %C%
+ping localhost -n 2 >nul
+cls
+echo The following project is powered by Star-ROM (C) 2016 Star Club
+echo Loading..
+echo %C%
+ping localhost -n 2 >nul
+cls
+echo The following project is powered by Star-ROM (C) 2016 Star Club
+echo Loading...
+echo %C%
+ping localhost -n 2 >nul
+cls
+set /a c +=1
+goto a
+:startE
 start flash.exe project.swf
